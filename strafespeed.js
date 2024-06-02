a77e8bce2f47b73c35cbdaa3ef85d17c300f56ca
Tygo <106909784+pyclicker@users.noreply.github.com>
2024-06-02 18:17:03 +0200

    Add files via upload

---
+++ strafespeed.js
@@ -0,0 +1,66 @@
+/*
+name: strafespeed
+author: Shoffli, Wallhacks and Tygozegthoi for the trigonometry
+*/
+const w = new KeyBind(17)
+const a = new KeyBind(30)
+const s = new KeyBind(31)
+const d = new KeyBind(32)
+var timer = Timer()
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+breeze.registerModule("Test", "Test.", {
+      speedtest: new DoubleSetting('Speedtest', 'Salalalla.', 1, 1, 10),
+      motion: function(event) {
+      var sprinting = 0
+      if (mc.getPlayer().isSprinting()) sprinting = 1
+      speed = (this.speedtest.getValue()*0.2805)
+      yaw = mc.getPlayer().getLastYaw()
+      deg = ((yaw-360*Math.floor(yaw/360)))
+      var fb = 0
+      var strafe = 0
+      if (w.isDown()) fb += 1
+      if (s.isDown()) fb -= 1
+      if (a.isDown()) strafe -= 1
+      if (d.isDown()) strafe += 1
+      if (fb == 0 && strafe == 0){
+         event.setX(0);
+         event.setZ(0);
+      }
+      if (fb == 1 && strafe == 0){
+         event.setX(-Math.sin(degrees_to_radians(deg))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg))*speed);
+      }
+      if (fb == -1 && strafe == 0){
+         event.setX(-Math.sin(degrees_to_radians(deg+180))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg+180))*speed);
+      }
+      if (fb == 1 && strafe == 1){
+         event.setX(-Math.sin(degrees_to_radians(deg+45))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg+45))*speed);
+      }
+      if (fb == 1 && strafe == -1){
+         event.setX(-Math.sin(degrees_to_radians(deg-45))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg-45))*speed);
+      }
+      if (fb == -1 && strafe == 1){
+         event.setX(-Math.sin(degrees_to_radians(deg+180-45))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg+180-45))*speed);
+      }
+      if (fb == -1 && strafe == -1){
+         event.setX(-Math.sin(degrees_to_radians(deg+180+45))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg+180+45))*speed);
+      }
+      if (fb == 0 && strafe == 1){
+         event.setX(-Math.sin(degrees_to_radians(deg+90))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg+90))*speed);
+      }
+      if (fb == 0 && strafe == -1){
+         event.setX(-Math.sin(degrees_to_radians(deg-90))*speed);
+         event.setZ(Math.cos(degrees_to_radians(deg-90))*speed);
+      }
+    }
+});
